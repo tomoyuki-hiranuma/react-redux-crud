3392ccb019d1dc709193830eb80b8ddb44cd5834
tomoyuki-hiranuma <numanumatom@gmail.com>
2021-03-18 19:16:24 +0900

    implement redux form

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { Field, reduxForm } from 'redux-form';
 
 import { postEvent } from '../actions';
 
 class EventsNew extends Component {
+  renderField(field) {
+    const { input, label, type, meta: { touched, error } } = field;
+    return (
+      <div>
+        <input {...input} placeholder={label} type="text" />
+        {touched && error && <span>{error}</span>}
+      </div>
+    );
+  }
+
   render() {
     return (
-      <React.Fragment>
-      </React.Fragment>
+      <form>
+        <div>
+          <Field label="Title" name="title" type='text' component={this.renderField} />
+        </div>
+        <div>
+          <Field label="Body" name="body" type='text' component={this.renderField} />
+        </div>
+        <div>
+          <input type="submit" value="Submit" disabled={false} />
+          <Link to='/'>Cancel</Link>
+        </div>
+      </form>
     )
   }
 }
 
+const validate = values => {
+  const errors = {}
+
+  if(!values.title) errors.title = "Enter a title, please";
+  if(!values.body) errors.body = "Enter a body. please";
+  return errors
+}
 // const mapDispatchToProps = ({ postEvents });
 
-export default connect(null, null)(EventsNew)
+export default connect(null, null)(
+  reduxForm({ validate, form: 'eventNewForm' })(EventsNew)
+)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,8 @@
 import { combineReducers } from 'redux';
 import eventsReducer from "./eventsReducer";
+import { reducer as formReducer } from 'redux-form';
 
-export default combineReducers({ eventsReducer })
+export default combineReducers({
+  eventsReducer,
+  form: formReducer,
+})
