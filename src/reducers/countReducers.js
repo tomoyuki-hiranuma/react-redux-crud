9c1f1d02a284f4842d8ca015a2a29fd4e61bb38a
tomoyuki-hiranuma <numanumatom@gmail.com>
2021-03-18 15:06:08 +0900

    add reducers

---
+++ src/reducers/countReducers.js
@@ -0,0 +1,19 @@
+import { DECREMENT, INCREMENT } from "../actions";
+
+const initialState = {
+  count: 0,
+};
+
+export default (state = initialState, action) => {
+  switch(action.type) {
+    case INCREMENT:
+      return {
+        count: state.count + 1
+      }
+
+    case DECREMENT:
+      return {
+        count: state.count - 1
+      }
+  }
+}
