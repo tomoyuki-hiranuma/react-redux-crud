2afb4a83411cfa9d9ec01cf901c25b5be52adddf
tomoyuki-hiranuma <numanumatom@gmail.com>
2021-03-18 15:20:19 +0900

    fix bug

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import reportWebVitals from './reportWebVitals';
 const store = createStore(reducer);
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>
 , document.getElementById('root'));
